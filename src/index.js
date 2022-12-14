3e2cffddc89721c018b6b0c0ea335121f5a426c5
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-12-14 17:10:46 +0200

    BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 import 'modern-normalize';
 // import './styles.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
